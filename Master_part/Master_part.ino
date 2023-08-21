d378db1afbdd72e144fc8906bbde42ccb6050b12
PranavSP0410 <sppranav269@gmail.com>
2023-08-21 20:29:08 +0530

    Seperated for clarity

--- Master_part/Master_part.ino
+++ Master_part/Master_part.ino
@@ -4,7 +4,7 @@ dht DHT_11;
 
 #define temp 13
 void setup() {
-  Serial.begin(38400);
+  Serial.begin(38400); 
 }
 
 void transmitter()
@@ -12,6 +12,7 @@ void transmitter()
   int val=DHT_11.read11(temp);
   int humid=DHT_11.humidity;
   int temper=DHT_11.temperature;
+  
   Serial.write(255);
   Serial.write(250);
   Serial.write(humid);
